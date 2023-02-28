903f3f332d8f067819fbf51dda0bba21341b2ab4
Hattstadt Quentin <hattstadtquentin@MacBook-Pro-de-Hattstadt.local>
2023-02-28 17:24:29 +0100

    Setting floating Ambula

--- src/components/Models/AmubulaModel.jsx
+++ src/components/Models/AmubulaModel.jsx
@@ -1,37 +1,56 @@
 import { useFrame, useLoader } from "@react-three/fiber";
 import { useRef } from "react";
-import { useGLTF, OrbitControls, useTexture } from "@react-three/drei";
+import {
+	useGLTF,
+	OrbitControls,
+	useTexture,
+	GradientTexture,
+} from "@react-three/drei";
 
-export default function AmbulaModel()
-{
-   // const modelRef = useRef(null);
-    const { nodes } = useGLTF('./AmbulaModel/ambulaModel.glb');
-    const bakedTexture = useTexture('./AmbulaModel/bakedAmbulaModel.jpg');
-    bakedTexture.flipY = false;
+export default function AmbulaModel() {
+	const ambulaRef = useRef();
 
-    console.log(nodes);
+	useFrame((state) => {
+		ambulaRef.current.rotation.z += 0.01;
+		ambulaRef.current.position.y =
+			Math.sin(state.clock.getElapsedTime()) * 0.5 + 0.5;
+	});
 
-    // useFrame(() => {
-    //     modelRef.current.rotation.y += 0.01
-    // });
+	// const modelRef = useRef(null);
+	const { nodes } = useGLTF("./AmbulaModel/ambulaModel.glb");
+	const bakedTexture = useTexture("./AmbulaModel/bakedAmbulaModel.jpg");
+	bakedTexture.flipY = false;
 
-    return <group dispose={null}>
-        <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Base.geometry}
-            scale={[0.27, 0.13, 0.27]}
-        />
-        <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Ambula.geometry}
-            position={[0, 0.32, 0]}
-            rotation={[Math.PI / 2, 0, 0]}
-            scale={1.29}
-        />
-    </group>
+	console.log(nodes);
+
+	// useFrame(() => {
+	//     modelRef.current.rotation.y += 0.01
+	// });
+
+	return (
+		<group dispose={null}>
+			<mesh
+				castShadow
+				receiveShadow
+				geometry={nodes.Base.geometry}
+				position={[1, -2, 0]}
+				scale={2}
+			>
+				<meshBasicMaterial map={bakedTexture} />
+			</mesh>
+			<mesh
+				ref={ambulaRef}
+				castShadow
+				receiveShadow
+				geometry={nodes.Ambula.geometry}
+				rotation={[Math.PI / 2, 0, 0]}
+				position={[1, 0.5, 0]}
+				scale={7}
+			>
+				<meshBasicMaterial map={bakedTexture} />
+			</mesh>
+		</group>
+	);
 }
 
 useGLTF.preload("./AmbulaModel/ambulaModel.glb");
-
--- src/routes/Landing/Landing.jsx
+++ src/routes/Landing/Landing.jsx
@@ -1,88 +1,111 @@
-import { useRef } from 'react';
-import lightVector from '@/assets/lightVector.svg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight, faAngleDown } from '@fortawesome/free-solid-svg-icons'
-import Github from '@/assets/github.svg';
-import Discord from '@/assets/discord.svg';
-import Mail from '@/assets/telegram-alt.svg';
-import Twitter from '@/assets/twitter.svg';
-import Youtube from '@/assets/youtube.svg';
+import { useRef } from "react";
+import lightVector from "@/assets/lightVector.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight, faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import Github from "@/assets/github.svg";
+import Discord from "@/assets/discord.svg";
+import Mail from "@/assets/telegram-alt.svg";
+import Twitter from "@/assets/twitter.svg";
+import Youtube from "@/assets/youtube.svg";
 import { motion } from "framer-motion";
-import TextAnim from '@/components/TextAnim/TextAnim';
-import { Canvas } from '@react-three/fiber';
+import TextAnim from "@/components/TextAnim/TextAnim";
+import { Canvas } from "@react-three/fiber";
 
-import './Landing.scss'
-import AmbulaModel from '../../components/Models/AmubulaModel';
+import "./Landing.scss";
+import AmbulaModel from "../../components/Models/AmubulaModel";
 
 function Landing() {
-  const screen2 = useRef(null);
-  const executeScroll = () => screen2.current.scrollIntoView({ behavior: 'smooth', block: 'nearest' });   
+	const screen2 = useRef(null);
+	const executeScroll = () =>
+		screen2.current.scrollIntoView({
+			behavior: "smooth",
+			block: "nearest",
+		});
 
-  return (
-    <div className="Landing">
-      <div className='landingContainer'>
-        <div className='leftContainer'>
-          <div className='title'>
-            <div className='t1'>
-              <TextAnim text="The first Blockchain using" />
-            </div>
-            <div className='t2'>
-              <TextAnim text="Proof of Interaction" />
-            </div>
-          </div>
-          <div className='content'>
-            Get started with a new client-puzzle called Proof-of-Interaction 
-            to define a new energy-efficient Blockchain protocol. 
-          </div>
-          <motion.button 
-            className='initiateButton'
-            whileHover={{
-              scale: 1.1,
-            }}
-            whileTap={{ scale: 0.9 }}
-          >
-            <span>Initiate the change</span>
-            <FontAwesomeIcon icon={faArrowRight} />
-          </motion.button>
-        </div>
-        <Canvas flat
-        camera={ {
-            fov: 30,
-            near: 1,
-            far: 50,
-            position: [ 12, 2, 3 ]
-        } }
-        className='ambulaModel'>
-            <AmbulaModel/>
-        </Canvas>
-      </div>
-      <div className='learnMoreContainer'>
-        <motion.div 
-          className='learnMore'
-          whileHover={{
-            scale: 1.1,
-          }}
-          whileTap={{ scale: 0.9 }}
-          onClick={executeScroll}
-        >
-          <span>Learn More</span>
-          <FontAwesomeIcon className="angle" icon={faAngleDown} bounce/>
-        </motion.div>
-        <div className='appLogoContainer'>
-          <img src={Youtube} className="appLogo youtube" alt="Youtube logo" />
-          <img src={Twitter} className="appLogo twitter" alt="Twitter logo" />
-          <img src={Mail} className="appLogo" alt="Mail logo" />
-          <img src={Discord} className="appLogo" alt="Discord logo" />
-          <img src={Github} className="appLogo github" alt="Github logo" />
-        </div>
-      </div>
-      <img src={lightVector} className="light" alt="Light vector" />
-      <img src={lightVector} className="light2" alt="Light vector" />
-      <div id="screen2" ref={screen2}>
-        &nbsp;
-      </div>
-    </div>
-  )
+	return (
+		<div className="Landing">
+			<div className="landingContainer">
+				<div className="leftContainer">
+					<div className="title">
+						<div className="t1">
+							<TextAnim text="The first Blockchain using" />
+						</div>
+						<div className="t2">
+							<TextAnim text="Proof of Interaction" />
+						</div>
+					</div>
+					<div className="content">
+						Get started with a new client-puzzle called
+						Proof-of-Interaction to define a new energy-efficient
+						Blockchain protocol.
+					</div>
+					<motion.button
+						className="initiateButton"
+						whileHover={{
+							scale: 1.1,
+						}}
+						whileTap={{ scale: 0.9 }}
+					>
+						<span>Initiate the change</span>
+						<FontAwesomeIcon icon={faArrowRight} />
+					</motion.button>
+				</div>
+				<Canvas
+					flat
+					camera={{
+						fov: 30,
+						near: 1,
+						far: 20,
+						position: [12, 5, 8],
+					}}
+					className="ambulaModel"
+				>
+					<AmbulaModel />
+				</Canvas>
+			</div>
+			<div className="learnMoreContainer">
+				<motion.div
+					className="learnMore"
+					whileHover={{
+						scale: 1.1,
+					}}
+					whileTap={{ scale: 0.9 }}
+					onClick={executeScroll}
+				>
+					<span>Learn More</span>
+					<FontAwesomeIcon
+						className="angle"
+						icon={faAngleDown}
+						bounce
+					/>
+				</motion.div>
+				<div className="appLogoContainer">
+					<img
+						src={Youtube}
+						className="appLogo youtube"
+						alt="Youtube logo"
+					/>
+					<img
+						src={Twitter}
+						className="appLogo twitter"
+						alt="Twitter logo"
+					/>
+					<img src={Mail} className="appLogo" alt="Mail logo" />
+					<img src={Discord} className="appLogo" alt="Discord logo" />
+					<img
+						src={Github}
+						className="appLogo github"
+						alt="Github logo"
+					/>
+				</div>
+			</div>
+			<img src={lightVector} className="light" alt="Light vector" />
+			<img src={lightVector} className="light2" alt="Light vector" />
+			<div id="screen2" ref={screen2}>
+				&nbsp;
+			</div>
+		</div>
+	);
 }
 
-export default Landing
+export default Landing;
