117445e9e6b976072b5f482d235e9c26f89853a1
Quentin HATTSTADT <qhattstadt@gmail.com>
2023-03-01 00:11:56 +0100

    WIP on Product page

--- src/routes/Product/Product.jsx
+++ src/routes/Product/Product.jsx
@@ -3,7 +3,7 @@ import reactLogo from '@/assets/react.svg'
 import lightVector from '@/assets/lightVector.svg'
 import AmbulaLogoShort from '@/assets/AmbulaLogoSmall.svg';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretUp } from "@fortawesome/free-solid-svg-icons";
+import { faCaretUp, faCaretDown, faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 import './Product.scss'
 
 function Landing() {
@@ -34,6 +34,72 @@ function Landing() {
           <div className='pub'>
             ADVERTISING 
             INSERT
+          </div>
+        </div>
+      </div>
+      <div className='cryptoStats'>
+        <div className='statsBarBackground'>
+          <div className='statsContainer'>
+            <div className='statsTitle'>
+              Market cap
+            </div>
+            <div className='statsContent'>
+              <span>$</span>
+              275,580,894
+            </div>
+            <div className='statsProgress up'>
+            <FontAwesomeIcon icon={faCaretUp} /> 0,50%
+            </div>
+          </div>
+          <div className='statsContainer'>
+            <div className='statsTitle'>
+              Fully Diluted
+            </div>
+            <div className='statsContent'>
+              <span>$</span>
+              274,736,646
+            </div>
+            <div className='statsProgress down'>
+            <FontAwesomeIcon icon={faCaretDown} /> 0,75%
+            </div>
+          </div>
+          <div className='statsContainer'>
+            <div className='statsTitle'>
+              Volume
+            </div>
+            <div className='statsContent'>
+              <span>$</span>
+              23,274,917
+            </div>
+            <div className='statsProgress up'>
+            
+            </div>
+          </div>
+          <div className='statsContainer'>
+            <div className='statsTitle'>
+              Circulating supply
+            </div>
+            <div className='statsContent'>
+              116,275,400
+              <span>AMB</span>
+            </div>
+            <div className='statsProgress amb'>
+              116,275,400
+            </div>
+          </div>
+        </div>
+        <div className='statsBar'></div>
+        <div className='statsBottom'>
+          <div className='statsOrder'>
+            <div className='orderTitle'>
+              Create Order 
+              <span><FontAwesomeIcon icon={faCircleInfo} /> How it works</span>
+            </div>
+            <div className='hr'>&nbsp;</div>
+          </div>
+          <div className='statsChart'>
+            <div className='statsChartBackground'></div>
+            <div className='statsChartBorder'></div>
           </div>
         </div>
       </div>
