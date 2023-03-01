205388eaecb3231fa372ddc92577b37b82820007
Quentin HATTSTADT <qhattstadt@gmail.com>
2023-03-01 07:31:13 +0100

    WIP on Product page

--- src/routes/Product/Product.jsx
+++ src/routes/Product/Product.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import reactLogo from '@/assets/react.svg'
 import lightVector from '@/assets/lightVector.svg'
+import violetLightVector from '@/assets/violetLightVector.svg';
 import AmbulaLogoShort from '@/assets/AmbulaLogoSmall.svg';
+import EtherumLogo from '@/assets/eth-logo.png';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretUp, faCaretDown, faCircleInfo } from "@fortawesome/free-solid-svg-icons";
+import BlockTranscationCard from '../../components/BlockTransactionCard/BlockTransactionCard';
 import './Product.scss'
 
 function Landing() {
@@ -15,7 +18,9 @@ function Landing() {
             Cryptocurrency | Coin
           </div>
           <div className='informationHeader'>
-            <div className='ambulaLogoSmall'><img src={AmbulaLogoShort} alt='Ambula Logo Small'/></div>
+            <div className='ambulaLogoSmall'>
+              <img src={AmbulaLogoShort} alt='Ambula Logo Small'/>
+            </div>
             <div className='informationContainer'>
               <div className='informationTitle'>
                 Ambula
@@ -96,11 +101,73 @@ function Landing() {
               <span><FontAwesomeIcon icon={faCircleInfo} /> How it works</span>
             </div>
             <div className='hr'>&nbsp;</div>
+            <div className='tradingContainer'>
+              Trading Pair
+              <div className='orderCardContainer'>
+                <div className='orderCard'>
+                  <div className='orderCardLeft'>
+                    <div className='orderCardImage'>
+                      <img src={AmbulaLogoShort} alt='Ambula Logo Small'/>
+                      <img src={lightVector} className="lightCard amb" alt="Light vector" />
+                    </div>
+                    <div className='orderInfos'>
+                      <div className='orderType'>
+                        Buy :
+                      </div>
+                      <div className='orderName amb'>
+                        AMBULA
+                      </div>
+                    </div>
+                  </div>
+                  <div className='orderCardRight'>
+                    <div className='orderNumber'>
+                      100
+                    </div>
+                    <div className='orderNumberDollar'>
+                      $40,36
+                    </div>
+                  </div>
+                </div>
+                <div className='orderCard'>
+                <div className='orderCardLeft'>
+                    <div className='orderCardImage'>
+                      <img src={EtherumLogo} alt='Etherum Logo'/>
+                      <img src={violetLightVector} className="lightCard" alt="Light vector" />
+                    </div>
+                    <div className='orderInfos'>
+                      <div className='orderType'>
+                        Sell :
+                      </div>
+                      <div className='orderName'>
+                        ETHERUM
+                      </div>
+                    </div>
+                  </div>
+                  <div className='orderCardRight'>
+                    <div className='orderNumber'>
+                      291.53
+                    </div>
+                    <div className='orderNumberDollar'>
+                      $40,36
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           <div className='statsChart'>
             <div className='statsChartBackground'></div>
             <div className='statsChartBorder'></div>
           </div>
+        </div>
+      </div>
+      <div className='blockTransactionContainer'>
+        <div className='blockTransactionWrapper'>
+          <div className='title'>LATEST BLOCKS</div>
+          <div className='blockTransactionCards'>
+            <BlockTranscationCard type='block'/>
+          </div>
+          <div className='seeAll'>View all <span>blocks</span></div>
         </div>
       </div>
       <img src={lightVector} className="light" alt="Light vector" />
