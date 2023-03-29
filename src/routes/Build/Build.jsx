9f99bbe25cdb32bbea961fa90ac3f9540eb1474a
Quentin Hattstadt <quentin.hattstadt@chargemap.com>
2023-03-29 16:19:54 +0200

    Section get started

--- src/routes/Build/Build.jsx
+++ src/routes/Build/Build.jsx
@@ -4,6 +4,9 @@ import AmbulaLogoLarge from "@/assets/AmbulaLogoLarge.svg";
 import "./Build.scss";
 import { motion } from "framer-motion";
 import { TypeAnimation } from "react-type-animation";
+import WaitingList from "@/assets/build/WaitingList.svg";
+import Message from "@/assets/build/Message.svg";
+import Configure from "@/assets/build/Configure.svg";
 
 function Build() {
 	const screen2 = useRef(null);
@@ -71,7 +74,45 @@ function Build() {
 					</div>
 				</div>
 			</div>
-			<div className="learnMoreContainer"></div>
+			<div className="learnMoreContainer">
+				<div className="learnMoreBackground"></div>
+				<div className="learnMoreTitle">
+					Quick Guide
+					<span>Get started together</span>
+				</div>
+				<div className="learnMoreSteps">
+					<div className="learnMoreCard card1">
+						<div className="cardTitle">Join the Waiting list</div>
+						<div className="imgContainer">
+							<img src={WaitingList} />
+						</div>
+						<div className="cardDescription">
+							To build an Ambula node, you first need to join the waitlist by signing up on the Ambula website. Once you've signed up,
+							you'll be added to the queue for node approvals.
+						</div>
+					</div>
+					<div className="learnMoreCard">
+						<div className="cardTitle">Wait the approval</div>
+						<div className="imgContainer">
+							<img className="img2" src={Message} />
+						</div>
+						<div className="cardDescription">
+							After joining the waitlist, you'll need to wait for approval from the Ambula team. They'll send you an email letting you
+							know if you've been approved to build a node or if you're still on the waiting list.
+						</div>
+					</div>
+					<div className="learnMoreCard">
+						<div className="cardTitle">Configure the node</div>
+						<div className="imgContainer">
+							<img className="img3" src={Configure} />
+						</div>
+						<div className="cardDescription">
+							Once you've received approval, it's time to configure your Ambula node. This involves setting up the hardware and
+							connecting to the Ambula network. Ambula provides detailed instructions to guide you through the process.
+						</div>
+					</div>
+				</div>
+			</div>
 			<img src={lightVector} className="light" alt="Light vector" />
 			<img src={lightVector} className="light2" alt="Light vector" />
 		</div>
